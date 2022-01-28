792b8976f5f5a8372cdf8e503ff58024255a8058
CityMado <stenius.jere@gmail.com>
2022-01-29 00:23:08 +0200

    wip

---
+++ Assets/Scripts/HouseDecorator/DebugPlayer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPlayer : MonoBehaviour
+{
+    public int playerCurrency = 5;
+    
+}
--- Assets/Scripts/HouseDecorator/HouseDecorator.cs
+++ Assets/Scripts/HouseDecorator/HouseDecorator.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class HouseDecorator : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
+    [SerializeField] private DebugPlayer player;
     public GameObject currentFurniture;
+    public GameObject currentBlueprint;
+    public int currentFurniturePrice;
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +34,11 @@ public class HouseDecorator : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0) && currentFurniture != null)
         {
+            if(player.playerCurrency >= currentFurniturePrice)
+            {
             Instantiate(currentFurniture, transform.position, Quaternion.identity);
+            player.playerCurrency -= currentFurniturePrice;
+            } else Debug.Log("poor bastard no money");
         }
     }
 }
