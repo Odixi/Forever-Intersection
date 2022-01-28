bc1f085f63f2f311a2fa9c63c8ce2ecdea691dc5
Odixi <vz4ev4r@gmail.com>
2022-01-28 19:35:50 +0200

    Scripti kansio ja muuta

---
+++ Assets/Scripts/WorldGneratorin/WorldBlock.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBlock : MonoBehaviour
+{
+    public Vector3Int BlockOrigin { get; set; }
+    public Vector3Int Size { get; private set; }
+
+}
