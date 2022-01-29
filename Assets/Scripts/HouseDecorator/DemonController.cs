ee52c7dd30e00f4b4e4f05987e20b5b81edba415
CityMado <stenius.jere@gmail.com>
2022-01-30 00:27:43 +0200

    RoomScene made more into game

---
+++ Assets/Scripts/HouseDecorator/DemonController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+    public Animator assAnimator, donutAnimator;
+    public float minimum = -1.0f;
+    public float maximum = 1.0f;
+    public float speed;
+    [Range(0,2)] float range = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveDemonUpAndDown();
+    }
+    void MoveDemonUpAndDown()
+    {
+        float y = Mathf.PingPong(Time.time * speed, 1) * range + 2f;
+        transform.position = new Vector3(0, y, 1.849f);
+    }
+}
