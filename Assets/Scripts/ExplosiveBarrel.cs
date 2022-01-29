48d7af2dc282926488f8c70db065a84a7c9113c1
Vaintti <Vaintti@users.noreply.github.com>
2022-01-30 01:21:04 +0200

    barrel damage things

--- Assets/Scripts/ExplosiveBarrel.cs
+++ Assets/Scripts/ExplosiveBarrel.cs
@@ -9,6 +9,7 @@ public class ExplosiveBarrel : MonoBehaviour
     [SerializeField]
     private Light light;
     private bool gotHit = false;
+    private float fallOffDistance = 5f;
 
     public void GetHit()
     {
@@ -17,11 +18,34 @@ public class ExplosiveBarrel : MonoBehaviour
         StartCoroutine(Explode());
     }
 
+    private void ExplosionDamage()
+    {
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+
+            var damage = Mathf.RoundToInt(110f - 110f * Vector3.Distance(player.transform.position, transform.position) / fallOffDistance);
+            player.GetComponent<Player>().TakeDamage(damage);
+        }
+        foreach (var enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            var damage = Mathf.RoundToInt(110f - Vector3.Distance(enemy.transform.position, transform.position) / fallOffDistance);
+            enemy.GetComponent<Enemy>().TakeDamage(damage);
+        }
+        foreach (var barrel in GameObject.FindGameObjectsWithTag("Barrel"))
+        {
+            if (Vector3.Distance(barrel.transform.position, transform.position) < 3)
+            {
+                barrel.GetComponent<ExplosiveBarrel>().GetHit();
+            }
+        }
+    }
+
     IEnumerator Explode()
     {
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         explosion.SetActive(true);
         light.intensity = 0;
+        ExplosionDamage();
         yield return new WaitForSeconds(2f);
         Destroy(gameObject);
         yield return null;
