4ea0e1f0f153382823f9b2e6d094fdcd3ab6a959
Odixi <vz4ev4r@gmail.com>
2022-01-29 13:02:51 +0200

    World generation

--- Assets/Scripts/WorldGneratorin/WorldBlock.cs
+++ Assets/Scripts/WorldGneratorin/WorldBlock.cs
@@ -1,10 +1,146 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
 
 public class WorldBlock : MonoBehaviour
 {
-    public Vector3Int BlockOrigin { get; set; }
-    public Vector3Int Size { get; private set; }
+    public Vector3Int BlockOrigin;
+    public Vector3Int Size;
 
+    // Open spaces and neighbors
+    public Vector3Int[] Openings;
+    public Vector3Int[] Directions { get; private set; }
+    public WorldBlock[] Neighbors { get; private set; }
+
+    private void Awake()
+    {
+        CalcualteDirections();
+    }
+
+    private void CalcualteDirections()
+    {
+        Directions = new Vector3Int[Openings.Length];
+        Neighbors = new WorldBlock[Openings.Length];
+        var sv = 0.5f * Vector3.one;
+        for (int i = 0; i < Openings.Length; i++)
+        {
+            Vector3Int op = Openings[i];
+            var mid = op + sv;
+            if (mid.x < 0)  {
+                Directions[i] = new Vector3Int(-1, 0, 0);
+            }
+            else if (mid.x > Size.x)  {
+                Directions[i] = new Vector3Int(1, 0, 0);
+            }
+            if (mid.y < 0)  {
+                Directions[i] = new Vector3Int(0, -1, 0);
+            }
+            else if (mid.y > Size.y)  {
+                Directions[i] = new Vector3Int(0, 1, 0);          
+            }
+            if (mid.z < 0) {
+                Directions[i] = new Vector3Int(0, 0, -1);
+            }
+            else if (mid.z > Size.z)          {
+                Directions[i] = new Vector3Int(0, 0, 1);
+            }
+        }
+    }
+
+    public void SetOrigin(Vector3Int origin)
+    {
+        BlockOrigin = origin;
+        transform.position = WorldGenerator.BlockSize * new Vector3(origin.x, origin.y, origin.z);
+    }
+
+    public void Rotate(Quaternion rot)
+    {
+        transform.rotation *= rot;
+        var newSize = rot * Size;
+        Size = new Vector3Int(Mathf.RoundToInt(newSize.x), Mathf.RoundToInt(newSize.y), Mathf.RoundToInt(newSize.z));
+        for (int i = 0; i < Openings.Length; ++i)
+        {
+            var no = rot * Openings[i];
+            Openings[i] = new Vector3Int(Mathf.RoundToInt(no.x), Mathf.RoundToInt(no.y), Mathf.RoundToInt(no.z));
+            var nd = rot * Directions[i];
+            Directions[i] = new Vector3Int(Mathf.RoundToInt(nd.x), Mathf.RoundToInt(nd.y), Mathf.RoundToInt(nd.z));
+        }
+        
+        // Get all child objects and do mysterious offset
+        if (Size.x < 0)
+        {
+            foreach (Transform child in transform)
+            {
+                child.position += Vector3.right*WorldGenerator.BlockSize;
+            }
+        }
+        if (Size.y < 0)
+        {
+            foreach (Transform child in transform)
+            {
+                child.position += Vector3.up * WorldGenerator.BlockSize;
+            }
+        }
+        if (Size.z < 0)
+        {
+            foreach (Transform child in transform)
+            {
+                child.position += Vector3.forward * WorldGenerator.BlockSize;
+            }
+        }
+    }
+
+    public List<Vector3Int> GetOccupiedSpaces()
+    {
+        List<Vector3Int> spaces = new List<Vector3Int>();
+        for (int x = Mathf.Min(Size.x+1, 0); x < Mathf.Max(Size.x, 1); ++x)
+        {
+            for (int y = Mathf.Min(Size.y+1, 0); y < Mathf.Max(Size.y, 1); ++y)
+            {
+                for (int z = Mathf.Min(Size.z+1, 0); z < Mathf.Max(Size.z, 1); ++z)
+                {
+                    spaces.Add(BlockOrigin + new Vector3Int(x, y, z));
+                }
+            }
+        }
+        return spaces;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (UnityEditor.Selection.activeObject != gameObject)
+        {
+            return;
+        }
+        var o = transform.position;
+        var s = WorldGenerator.BlockSize;
+        foreach (var op in Openings)
+        {
+            Vector3[] corners = new Vector3[]
+            {
+                new Vector3(op.x,   op.y,   op.z  ),
+                new Vector3(op.x+1, op.y,   op.z  ),
+                new Vector3(op.x,   op.y+1, op.z  ),
+                new Vector3(op.x,   op.y,   op.z+1),
+                new Vector3(op.x+1, op.y+1, op.z  ),
+                new Vector3(op.x  , op.y+1, op.z+1),
+                new Vector3(op.x+1, op.y  , op.z+1),
+                new Vector3(op.x+1, op.y+1, op.z+1),
+            };
+            //var toDraw = corners.Where(a => a.x >= Mathf.Min(Size.x, 0) && a.x <= Mathf.Max(Size.x, 0) &&
+            //                                a.y >= Mathf.Min(Size.y, 0) && a.y <= Mathf.Max(Size.y, 0) &&
+            //                                a.z >= Mathf.Min(Size.z, 0) && a.z <= Mathf.Max(Size.z, 0)  ).ToArray();
+            for (int i = 0; i < corners.Count(); ++i)
+            {
+                for (int j = i+1; j < corners.Count(); ++j)
+                {
+                    Gizmos.DrawLine(o + corners[i]*s, o + corners[j]*s);
+                }
+            }
+        }
+    }
+
+   
 }
--- Assets/Scripts/WorldGneratorin/WorldGenerator.cs
+++ Assets/Scripts/WorldGneratorin/WorldGenerator.cs
@@ -1,18 +1,158 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class WorldGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static readonly float BlockSize = 2;
+
+    public List<WorldBlock> Blocks { get; private set; } = new List<WorldBlock>();
+    public HashSet<Vector3Int> OccupiedSpaces { get; private set; } = new HashSet<Vector3Int>();
+    [SerializeField]
+    private bool dbugOccupied = false;
+
+    [SerializeField]
+    private List<GameObject> BlockTemplates;
+    [SerializeField]
+    private GameObject StartBlock;
+
+    private bool cont = false;
+
+    private void Start()
     {
-        
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
-        
+        Blocks = new List<WorldBlock>();
+        OccupiedSpaces = new HashSet<Vector3Int>();
+
+        var go = Instantiate(StartBlock);
+        go.transform.rotation = Quaternion.identity;
+        go.transform.position = Vector3Int.zero;
+        var block = go.GetComponent<WorldBlock>();
+        block.SetOrigin(Vector3Int.zero);
+        Blocks.Add(block);
+        AppendBlockToOccupiedSpaces(block);
+
+        PopulateBlockOpenings(block, 1);
+
+        //StartCoroutine(PopulateBlockOpeningsCr(block, 1));
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cont = true;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            foreach(var b in Blocks)
+            {
+                Destroy(b.gameObject);
+            }
+            Init();
+        }
+    }
+
+    private void PopulateBlockOpenings(WorldBlock block, int depth)
+    {
+        if (depth > 20)
+        {
+            return;
+        }
+        for (int i = 0; i < block.Openings.Length; ++i)
+        {
+            if (block.Neighbors[i] != null){
+                continue;
+            }
+            var newBlock = AddBlock(BlockTemplates[Random.Range(0, BlockTemplates.Count)], block, i);
+            if (newBlock == null)
+            {
+                // TODO add some kind of wall
+                continue;
+            }
+            PopulateBlockOpenings(newBlock, depth + 1);
+        }
+    }
+
+    private IEnumerator PopulateBlockOpeningsCr(WorldBlock block, int depth)
+    {
+        if (depth <= 5)
+        {
+            for (int i = 0; i < block.Openings.Length; ++i)
+            {
+                if (block.Neighbors[i] != null)
+                {
+                    continue;
+                }
+                yield return new WaitUntil(() => cont);
+                cont = false;
+                var newBlock = AddBlock(BlockTemplates[Random.Range(0, BlockTemplates.Count)], block, i);
+                yield return StartCoroutine(PopulateBlockOpeningsCr(newBlock, depth + 1));
+            }
+        }
+    }
+
+    private WorldBlock AddBlock(GameObject pref, WorldBlock neighbor, int openingIndex)
+    {
+        var go = Instantiate(pref);
+        go.transform.rotation = Quaternion.identity;
+        var block = go.GetComponent<WorldBlock>();
+
+        // Take random index
+        int newIndex = UnityEngine.Random.Range(0, block.Openings.Length);
+
+        var newDir = block.Directions[newIndex];
+        var neighborDir = neighbor.Directions[openingIndex];
+
+        //var rot = Quaternion.FromToRotation(newDir, -neighborDir);
+        var angle = Vector3.SignedAngle(newDir, -neighborDir, Vector3.up);
+        Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
+
+        block.Rotate(rot);
+        block.SetOrigin(neighbor.BlockOrigin + neighbor.Openings[openingIndex] - neighbor.Directions[openingIndex] - block.Openings[newIndex]);
+
+        if (block.GetOccupiedSpaces().Any(x => OccupiedSpaces.Contains(x))){
+            Destroy(go);
+            return null;
+        }
+
+        AppendBlockToOccupiedSpaces(block);
+        neighbor.Neighbors[openingIndex] = block;
+        block.Neighbors[newIndex] = neighbor;
+        Blocks.Add(block);
+        return block;
+    }
+
+    private void AppendBlockToOccupiedSpaces(WorldBlock block)
+    {
+        foreach(var v in block.GetOccupiedSpaces()){
+            OccupiedSpaces.Add(v);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (dbugOccupied)
+        {
+            Gizmos.color = Color.red;
+            foreach(var space in OccupiedSpaces)
+            {
+                var sf = new Vector3(space.x, space.y, space.z) * BlockSize;
+                Gizmos.DrawLine(sf, 
+                                sf + Vector3.one * BlockSize);
+                Gizmos.DrawLine(sf + new Vector3(1, 0, 0)*BlockSize, 
+                                sf + (Vector3.one - new Vector3(1, 0, 0)) * BlockSize);
+                Gizmos.DrawLine(sf + new Vector3(0, 1, 0)*BlockSize, 
+                                sf + (Vector3.one - new Vector3(0, 1, 0)) * BlockSize);
+                Gizmos.DrawLine(sf + new Vector3(0, 0, 1)*BlockSize, 
+                                sf + (Vector3.one - new Vector3(0, 0, 1)) * BlockSize);
+            }
+        }
+    }
+
 }
