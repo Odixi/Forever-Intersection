6a3cfa31bbfaf02b82fafb0af701bf58c4289ae0
CityMado <stenius.jere@gmail.com>
2022-01-29 21:21:04 +0200

    Added Face animations

--- Assets/Scripts/FPS/HUDElementController.cs
+++ Assets/Scripts/FPS/HUDElementController.cs
@@ -9,6 +9,7 @@ public class HUDElementController : MonoBehaviour
     [SerializeField] private Animator animator;
     public TextMeshProUGUI ammoCurrentMag, ammoAll, health, gibleds;
     public Player player;
+    public float debugHealth;
     public Weapon weapon;
 
     // Start is called before the first frame update
@@ -21,13 +22,33 @@ public class HUDElementController : MonoBehaviour
     void Update()
     {
         UpdatePlayerResources();
+        ChangePlayerFace();
     }
     void UpdatePlayerResources()
     {
         health.text = player.Health.ToString();
-
         ammoCurrentMag.text = weapon.ammoInMagazine.ToString();
-        ammoAll.text = weapon.ammo.ToString();
-        
+        ammoAll.text = weapon.ammo.ToString();      
+    }
+    void ChangePlayerFace()
+    {
+        if(debugHealth >= 80f)
+        {
+            animator.SetBool("Full", true);
+            animator.SetBool("Medium", false);
+            animator.SetBool("Low", false);
+        }
+        if(debugHealth >= 30 && debugHealth <= 79f)
+        {
+            animator.SetBool("Full", false);
+            animator.SetBool("Medium", true);
+            animator.SetBool("Low", false);
+        }
+        if(debugHealth >= 0 && debugHealth <= 29f)
+        {
+            animator.SetBool("Full", false);
+            animator.SetBool("Medium", false);
+            animator.SetBool("Low", true);
+        }
     }
 }
